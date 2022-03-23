cca7aa87cabd6543cf1edd3da4bbe9a36580ae82
NikhilPatil07 <patilnikhil181804@gmail.com>
2022-03-23 09:37:17 +0530

    ADDED:[Nikhil] Sub for 2 Numbers

---
+++ SUB.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter The 2 Number :");
+	scanf("%d %d",&a,&b);
+	c=a-b;
+	printf(" Sub :%d",c);
+	getch();
+
+}
