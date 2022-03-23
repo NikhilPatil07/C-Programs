bedc361f18305e67eccfd30dccbca8f8b618ffdb
NikhilPatil07 <patilnikhil181804@gmail.com>
2022-03-23 09:26:11 +0530

    ADDED:[Nikhil] Addition of 2 Numbers

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter The 2 Number :");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n Add :%d",c);
+	getch();
+}
